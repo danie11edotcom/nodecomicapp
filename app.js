2262070fed16706830a882c58f482c9f7e30ee76
Danielle Hill <goal2code@gmail.com>
2015-07-16 17:54:01 -0400

    Updated plan details

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 //Solution: Build a command line app to retrieve Marvel character summary for given name using Node.js and Marvel API
 
 //Plan
-//Connect to the API URL 
-//Read the data
+//Connect to the API URL (http://gateway.marvel.com/v1/public/characters)
+//Read the data (description is in results array -->results.description?)
 //Parse the data
-//Print data
+//Print data and Marvel's attribution ('Data provided by Marvel. (c) 2014 Marvel')
 
