17e76c4afdce2125cd2bcf81c7ae183982aaf800
Danielle Hill <goal2code@gmail.com>
2015-07-16 17:23:03 -0400

    Added planning and preparation comments

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-console.log('Hello world!');
-console.log('Hello world again!');
+//Prepare
+//Problem:  A simple way to find Marvel comic character descriptions is needed
+//Solution: Build a command line app to retrieve Marvel character summary for given name using Node.js and Marvel API
+
+//Plan
+//Connect to the API URL 
+//Read the data
+//Parse the data
+//Print data
+
