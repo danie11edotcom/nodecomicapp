f4cd59c29aa5ab646c240de890f67b2e06460baa
Danielle Hill <goal2code@gmail.com>
2015-07-17 20:39:18 -0400

    Initial commit for comic.js

---
+++ comic.js
@@ -0,0 +1,11 @@
+//Prepare
+//Problem:  A simple way to find Marvel comic character descriptions is needed
+//Solution: Build a command line app to retrieve Marvel character summary for given name using Node.js and Marvel API
+
+//Plan
+//Connect to the API URL (http://gateway.marvel.com/v1/public/characters)
+//Read the data (description is in results array -->results.description?)
+//Parse the data
+//Print data and Marvel's attribution ('Data provided by Marvel. (c) 2014 Marvel')
+
+//Use module.exports to export function to get comic character info
