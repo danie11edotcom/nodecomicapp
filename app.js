0c00b920c31bac6db91b797dbd84dcf7e264bf44
Danielle Hill <goal2code@gmail.com>
2015-07-17 20:38:57 -0400

    Added require for comic file

--- app.js
+++ app.js
@@ -1,13 +1,3 @@
-//Prepare
-//Problem:  A simple way to find Marvel comic character descriptions is needed
-//Solution: Build a command line app to retrieve Marvel character summary for given name using Node.js and Marvel API
-
-//Plan
-//Connect to the API URL (http://gateway.marvel.com/v1/public/characters)
-//Read the data (description is in results array -->results.description?)
-//Parse the data
-//Print data and Marvel's attribution ('Data provided by Marvel. (c) 2014 Marvel')
-
-//test config file reading for API keys
-
-var config = require('./config/config.js');
+//Require config file for API keys
+var config = require('./config/config');
+var comic = require('./comic');
