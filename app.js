5983b6a6be011c60d2e815237b85eba278790c7a
Danielle Hill <goal2code@gmail.com>
2015-07-20 18:35:58 -0400

    Added response request

--- app.js
+++ app.js
@@ -1,5 +1,40 @@
+//Require http
+var http = require("http");
+
 //Require config file for API keys
 var config = require('./config/config');
 
 //Require comic file for method to get character data
 var comic = require('./comic');
+
+//Prepare
+//Problem:  A simple way to find Marvel comic character descriptions is needed
+//Solution: Build a command line app to retrieve Marvel character summary for given name using Node.js and Marvel API
+
+var character = "spider-man";
+var key = config.key_pub;
+
+function printMessage(character, description) {
+	var message = character + "'s description is: " + description;
+	console.log(message);
+}
+
+//Plan
+//Connect to the API URL (http://gateway.marvel.com:80/v1/public/characters?name=character&apikey=publickey)
+var request = http.get("http://gateway.marvel.com:80/v1/public/characters?name=" + character + "&apikey=" + key, function(response){
+	console.dir(responseStatusCode);
+	
+	//Read the data (description is in results array -->results.description?)
+
+
+	//Parse the data
+
+
+	//Print data and Marvel's attribution ('Data provided by Marvel. (c) 2014 Marvel') -- use attributionText from response
+	//printMessage(character, "He's a really cool dude!");
+
+
+});
+
+
+//Use module.exports to export function to get comic character info
