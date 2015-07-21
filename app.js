c9e182e587a4b648d18d97517fb75d8192025063
Danielle Hill <goal2code@gmail.com>
2015-07-21 18:26:20 -0400

    Added error message on request

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ var http = require("http");									//http for get method to connect to API
 var crypto = require('crypto');							//crypto for md5 hash
 var config = require('./config/config');		//config file for API keys 
 
-
-
 //Prepare
 //Problem:  A simple way to find Marvel comic character descriptions is needed
 //Solution: Build a command line app to retrieve Marvel character summary for given name using Node.js and Marvel API
@@ -17,7 +15,6 @@ var time_stamp = Math.floor(new Date() / 1000);
 var md5String = time_stamp + key_priv + key_pub;
 var hash = crypto.createHash('md5').update(md5String).digest('hex');
 
-
 function printMessage(character, description) {
 	var message = character + "'s description is: " + description;
 	console.log(message);
@@ -27,7 +24,7 @@ function printMessage(character, description) {
 //Connect to the API URL (http://gateway.marvel.com:80/v1/public/characters?name=character&apikey=publickey)
 var request = http.get("http://gateway.marvel.com:80/v1/public/characters?ts=" + time_stamp + "&apikey=" + key_pub + "&hash=" + hash, 
 	  function(response){
-			console.dir(response.StatusCode);
+			console.dir(response.statusCode);
 	
 			//Read the data (description is in results array -->results.description?)
 			response.on('data', function (chunk) {
@@ -43,5 +40,8 @@ var request = http.get("http://gateway.marvel.com:80/v1/public/characters?ts=" +
 
 });
 
+request.on('error', function(error){
+	//console.error(error.message);
+});
 
 //Use module.exports to export function to get comic character info
