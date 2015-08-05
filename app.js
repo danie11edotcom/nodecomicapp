b9db635ec2a50cd76765756595281f34c1030732
Danielle Hill <goal2code@gmail.com>
2015-08-05 19:18:31 -0400

    Added comments for improvement ideas

--- app.js
+++ app.js
@@ -2,3 +2,8 @@
 var comic = require('./comic');
 
 comic.get("spider-man");  //change to argv input
+
+//Improvement Ideas
+	//Add prompt to allow user to continue to interact with app
+	//Create separate module for print functions and require those in comic.js
+	//Add message for blank descrition on a valid character
