e00027da78157bb09381b2da9fa3f5dee688dd75
Danielle Hill <goal2code@gmail.com>
2015-08-05 20:23:51 -0400

    Commented out printAttribution function

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ var comic = require('./comic');
 var print = require('./print');
 
 var characters = process.argv.slice(2);
-
 characters.forEach(comic.get);  //change to argv input
-print.printAttribution('Hello world');
+//print.printAttribution('Hello world');
 
 //Improvement Ideas
 	//Add prompt to allow user to continue to interact with app
