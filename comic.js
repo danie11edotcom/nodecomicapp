e220f433d28584de512bdf0272f50566aea980af
Danielle Hill <goal2code@gmail.com>
2015-08-05 20:44:56 -0400

    Updated print function definitions

--- comic.js
+++ comic.js
@@ -12,12 +12,14 @@ var hash = crypto.createHash('md5').update(md5String).digest('hex');
 
 //Print out message
 function printMessage(character, description, attribution) {
-	var message = character + ": " + "\n" + description + "\n" +attribution;
+	var message = "\n" + character + ": " + "\n" + description + "\n" +attribution;
 	console.log(message);
 } 
 
-function printBlankDes(description) {
-	console.log("Sorry, no description exists for this Marvel character.");
+//Print out notification that description does not exist for character
+function printBlankDes(character) {
+	var noDescription = "\n" + character + " does not have a description.";
+	console.log(noDescription);
 }
 
 //Print out error message
@@ -41,8 +43,12 @@ function get(character) {
 						try {
 							//Parse the data (description is in results object array)
 							var characterData = JSON.parse(body);
-							//use name from response instead of name entered for correct capitalization and punctuation
-							printMessage(characterData["data"].results[0].name, characterData["data"].results[0].description, characterData["attributionText"]);  
+								if (characterData["data"].results[0].description !== "") {
+									//use name from response instead of name entered for correct capitalization and punctuation
+									printMessage(characterData["data"].results[0].name, characterData["data"].results[0].description, characterData["attributionText"]);  
+								} else { 
+									printBlankDes(characterData["data"].results[0].name);
+								}
 						} catch (error) {
 							//Parse error
 							printError(error);
