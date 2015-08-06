c2135bef4ab9326d180a65b151088f6716db922e
Danielle Hill <goal2code@gmail.com>
2015-08-05 20:47:07 -0400

    Update improvement ideas comments

--- app.js
+++ app.js
@@ -9,4 +9,3 @@ characters.forEach(comic.get);  //change to argv input
 //Improvement Ideas
 	//Add prompt to allow user to continue to interact with app
 	//Create separate module for print functions and require those in comic.js
-	//Add message for blank descrition on a valid character
