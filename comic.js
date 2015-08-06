e894195bd8b009b437182936ed78d65bca388297
Danielle Hill <goal2code@gmail.com>
2015-08-05 21:01:27 -0400

    Removed print function declarations

--- comic.js
+++ comic.js
@@ -2,6 +2,7 @@
 var http = require("http");									//http for get method to connect to API
 var crypto = require('crypto');							//crypto for md5 hash
 var config = require('./config/config');		//config file for API keys 
+var print = require('./print');							//print for functions to print messages to user
 
 //Variables
 var key_pub = config.key_pub;
@@ -10,23 +11,7 @@ var time_stamp = Math.floor(new Date() / 1000);
 var md5String = time_stamp + key_priv + key_pub;
 var hash = crypto.createHash('md5').update(md5String).digest('hex');
 
-//Print out message
-function printMessage(character, description, attribution) {
-	var message = "\n" + character + ": " + "\n" + description + "\n" +attribution;
-	console.log(message);
-} 
-
-//Print out notification that description does not exist for character
-function printBlankDes(character) {
-	var noDescription = "\n" + character + " does not have a description.";
-	console.log(noDescription);
-}
-
-//Print out error message
-function printError (error) {
-	console.error(error.message);
-}
-
+//Function to get character data from Marvel API
 function get(character) {
 	//Connect to the API URL
 	var request = http.get("http://gateway.marvel.com:80/v1/public/characters?name=" + character +"&ts=" + time_stamp + "&apikey=" + key_pub + "&hash=" + hash, 
@@ -45,23 +30,23 @@ function get(character) {
 							var characterData = JSON.parse(body);
 								if (characterData["data"].results[0].description !== "") {
 									//use name from response instead of name entered for correct capitalization and punctuation
-									printMessage(characterData["data"].results[0].name, characterData["data"].results[0].description, characterData["attributionText"]);  
+									print.printMessage(characterData["data"].results[0].name, characterData["data"].results[0].description, characterData["attributionText"]);  
 								} else { 
-									printBlankDes(characterData["data"].results[0].name);
+									print.printBlankDes(characterData["data"].results[0].name);
 								}
 						} catch (error) {
 							//Parse error
-							printError(error);
+							print.printError(error);
 							console.log(response.statusCode);
 						}
 					} else {
 						//Status code error
-						printError({message: "There was an error getting the profile for " + character + ". (" + http.STATUS_CODES[response.statusCode] + ")"})
+						print.printError({message: "There was an error getting the profile for " + character + ". (" + http.STATUS_CODES[response.statusCode] + ")"})
 					}
 				});	
 	});
 	//Connection error
-	request.on('error', printError);
+	request.on('error', print.printError);
 }
 
 //Use module.exports to export function to get comic character info
