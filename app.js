8ebeec814fbdf993787c38087b8176be34ef4e25
Danielle Hill <goal2code@gmail.com>
2015-08-05 21:02:43 -0400

    Updated comments

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var comic = require('./comic');
 var print = require('./print');
 
 var characters = process.argv.slice(2);
-characters.forEach(comic.get);  //change to argv input
-//print.printAttribution('Hello world');
+characters.forEach(comic.get);
 
 //Improvement Ideas
 	//Add prompt to allow user to continue to interact with app
