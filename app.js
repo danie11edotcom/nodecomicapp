076d0fc7c4b88177d877e6c075e907c7918f5109
Danielle Hill <goal2code@gmail.com>
2015-08-05 21:07:19 -0400

    Removed completed improvement idea

--- app.js
+++ app.js
@@ -7,5 +7,4 @@ characters.forEach(comic.get);
 
 //Improvement Ideas
 	//Add prompt to allow user to continue to interact with app
-	//Create separate module for print functions and require those in comic.js
 	//Print attribution once for multiple character names printed
