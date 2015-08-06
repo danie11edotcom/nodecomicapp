81e87d04b42edbd80a715cc4cd0787fe6c116d76
Danielle Hill <goal2code@gmail.com>
2015-08-05 21:01:51 -0400

    Added print function declarations

--- print.js
+++ print.js
@@ -1,10 +1,26 @@
 //Print functions
 
 var print = {
+										//Print attribution
 	printAttribution: function (attribution) {
 											var message = attribution;
 											console.log(attribution);
 										},
+										//Print character name, description and attribution
+	printMessage:  		function (character, description, attribution) {
+											var message = "\n" + character + ": " + "\n" + description + "\n" +attribution;
+											console.log(message);
+										},
+
+										//Print out notification that description does not exist for character
+	printBlankDes:		function (character) {
+											var noDescription = "\n" + character + " does not have a description.";
+											console.log(noDescription);
+										},
+										//Print out error message
+	printError: 			function (error) {
+											console.error(error.message);
+										}
 };
 
 module.exports = print;
