5ce1495e0b79196fe187fbd313515cae91944a7f
Danielle Hill <goal2code@gmail.com>
2015-08-05 20:08:11 -0400

    Added print module

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //Require Section
 var comic = require('./comic');
+var print = require('./print');
+
 var characters = process.argv.slice(2);
 
 characters.forEach(comic.get);  //change to argv input
-console.log(characters);
+print.printAttribution('Hello world');
 
 //Improvement Ideas
 	//Add prompt to allow user to continue to interact with app
