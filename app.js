f1368442e9c597695c03510eb015b9f2893ad50f
Danielle Hill <goal2code@gmail.com>
2015-08-21 20:29:07 -0400

    Removed future improvements from app.js; these are documented in Readme

--- app.js
+++ app.js
@@ -4,8 +4,3 @@ var print = require('./print');
 
 var characters = process.argv.slice(2);
 characters.forEach(comic.get);
-
-//Improvement Ideas
-    //Add prompt to allow user to continue to interact with app
-    //Print attribution once for multiple character names printed
-
